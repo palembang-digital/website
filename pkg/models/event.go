684cd19bed87dea6c6fcc72e525fb57b117e0c7b
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2020-08-26 01:07:07 +0700

    Make scheduled times omitempty
    Co-authored-by: apranta <reziapriliansyah@gmail.com>

--- pkg/models/event.go
+++ pkg/models/event.go
@@ -11,6 +11,6 @@ type Event struct {
 	ImageURL        string `json:"image_url" db:"image_url" valid:"url,required"`
 	RegistrationURL string `json:"registration_url" db:"registration_url" valid:"url"`
 
-	ScheduledStart *time.Time `json:"scheduled_start" db:"scheduled_start" example:"2020-04-21T00:00:00Z" valid:"required"`
-	ScheduledEnd   *time.Time `json:"scheduled_end" db:"scheduled_end" example:"2020-04-21T00:00:00Z" valid:"required"`
+	ScheduledStart *time.Time `json:"scheduled_start,omitempty" db:"scheduled_start" example:"2020-04-21T00:00:00Z" valid:"required"`
+	ScheduledEnd   *time.Time `json:"scheduled_end,omitempty" db:"scheduled_end" example:"2020-04-21T00:00:00Z" valid:"required"`
 }
