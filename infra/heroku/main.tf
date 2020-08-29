1408effd70f2b09c2c84190e9969c9adabf109ee
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2020-08-29 21:25:15 +0700

    Provision staging app and db in Heroku (#36)
    Co-authored-by: maximalyono <maximalyono@gmail.com>
    
    Co-authored-by: maximalyono <maximalyono@gmail.com>

--- infra/heroku/main.tf
+++ infra/heroku/main.tf
@@ -1,11 +1,3 @@
-variable "app_name" {
-  default = "palembang-digital"
-}
-
-variable "region" {
-  default = "us"
-}
-
 resource "heroku_app" "palembang_digital_app" {
   name   = var.app_name
   region = var.region
@@ -21,5 +13,23 @@ output "palembang_digital_app_url" {
 
 resource "heroku_addon" "palembang_digital_db" {
   app  = "${heroku_app.palembang_digital_app.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "heroku_app" "palembang_digital_staging_app" {
+  name   = var.app_name_staging
+  region = var.region
+
+  buildpacks = [
+    "heroku/go"
+  ]
+}
+
+output "palembang_digital_staging_app_url" {
+  value = "https://${heroku_app.palembang_digital_staging_app.name}.herokuapp.com"
+}
+
+resource "heroku_addon" "palembang_digital_staging_db" {
+  app  = "${heroku_app.palembang_digital_staging_app.name}"
   plan = "heroku-postgresql:hobby-dev"
 }
