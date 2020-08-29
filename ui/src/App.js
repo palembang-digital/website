de4591fd9408336ff489ced52f5b11aca38d9a5b
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2020-08-29 23:33:38 +0700

    Do not lazyload Landing page. It's already fast enough
    Co-authored-by: azhry <arliansyah_azhary@yahoo.com>

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import loadable from "@loadable/component";
 import { Router } from "@reach/router";
+import Home from "./Home";
 
-const Home = loadable(() => import("./Home"));
 const Team = loadable(() => import("./Team"));
 const Admin = loadable(() => import("./pages").then((c) => c.Admin));
 
