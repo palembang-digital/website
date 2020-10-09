d4edae1a07d85c01c5efe8e5d835b29e27db3b12
maximalyono <maximalyono@gmail.com>
2020-10-09 20:06:12 +0700

    edit file variables.tf, hapus variable www

--- infra/cloudflare/variables.tf
+++ infra/cloudflare/variables.tf
@@ -2,10 +2,6 @@ variable "zone-id" {
   default = ${{ secrets.PALEMBANGDIGITALORG_ZONE_ID }} 
 }
 
-variable "www" {
-  default = "palembang-digital.herokuapp.com"
-}
-
 variable "staging" {
   default = "palembang-digital-staging.herokuapp.com"
 }
