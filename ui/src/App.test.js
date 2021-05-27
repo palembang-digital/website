5b882ce9fb342bd712228f5c95b6ab2cbbb6d506
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2021-05-28 03:53:32 +0700

    Remove test

--- ui/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders landing page", () => {
-  const { getByText } = render(<App />);
-  const linkDaftarSekarang = getByText(/Daftar Sekarang/);
-  expect(linkDaftarSekarang).toBeInTheDocument();
-});
