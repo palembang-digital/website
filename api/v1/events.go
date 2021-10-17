c9b9dea08701fedbbf9124cebe428e481bd42de5
kmhalpin <kmhapinn3@gmail.com>
2021-10-17 20:25:29 +0700

    added update event api

--- api/v1/events.go
+++ api/v1/events.go
@@ -80,6 +80,41 @@ func (api *API) createEvent(c echo.Context) error {
 	return c.JSON(http.StatusCreated, newEvent)
 }
 
+// Update an event
+// @Summary Update an event
+// @Description Update an event
+// @Tags events
+// @ID update-event
+// @Produce json
+// @Param id path int true "Event ID"
+// @Param event body models.Event true "Update event"
+// @Success 200 {object} models.Event
+// @Router /events/{id} [put]
+func (api *API) updateEvent(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	idString := c.Param("id")
+	id, _ := strconv.ParseInt(idString, 10, 64)
+
+	event := new(models.Event)
+	if err := c.Bind(event); err != nil {
+		return err
+	}
+
+	if err := c.Validate(event); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	event.ID = id
+
+	updatedEvent, err := api.eventsService.UpdateEvent(ctx, *event)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, updatedEvent)
+}
+
 // Delete an event
 // @Summary Delete an event
 // @Description Delete an event by id
