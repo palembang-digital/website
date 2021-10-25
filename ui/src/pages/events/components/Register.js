bde6d963a1f93a9de538cd9c8c83630485a7a9c7
FadellMuhammad <fadel54031@gmail.com>
2021-10-25 12:12:28 +0700

    update code event-detail

--- ui/src/pages/events/components/Register.js
+++ ui/src/pages/events/components/Register.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Fragment } from "react";
 import iconDate from '../../../assets/icons/UpcomingIcons/Date.png'
-// import iconFree from '../../../assets/icons/UpcomingIcons/Free.png'
 import iconPayments from '../../../assets/icons/UpcomingIcons/payments.png'
-// import iconLocation from '../../../assets/icons/UpcomingIcons/Location.png'
 import iconTime from '../../../assets/icons/UpcomingIcons/Time.png'
 
 const Register = (props) => {
@@ -57,7 +55,6 @@ const Register = (props) => {
                 <p><img src={iconDate} alt="iconDate" />{FormatDateEvent()}</p>
                 <p><img src={iconTime} alt="iconTime" />{FormatTimeEvent()} WIB</p>
                 <p><img src={iconPayments} alt="iconPaid" />{props.fee}</p>
-                {/* <p><img src={iconLocation} alt="iconLocation" />  Live via Zoom Meet</p> */}
             </div>
         </Fragment>
     )
