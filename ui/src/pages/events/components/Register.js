0320c138872f8c0846859ec27d17cce6a77156cb
Chika <chika@pop-os.localdomain>
2021-11-17 23:06:31 +0700

    updating icon carousel, deleting folder test image, translate months name to indo in register.js

--- ui/src/pages/events/components/Register.js
+++ ui/src/pages/events/components/Register.js
@@ -1,66 +1,85 @@
 import React from "react";
 import { Fragment } from "react";
-import iconDate from '../../../assets/icons/UpcomingIcons/Date.png'
+import iconDate from "../../../assets/icons/UpcomingIcons/Date.png";
 // import iconFree from '../../../assets/icons/UpcomingIcons/Free.png'
-import iconPayments from '../../../assets/icons/UpcomingIcons/payments.png'
+import iconPayments from "../../../assets/icons/UpcomingIcons/payments.png";
 // import iconLocation from '../../../assets/icons/UpcomingIcons/Location.png'
-import iconTime from '../../../assets/icons/UpcomingIcons/Time.png'
+import iconTime from "../../../assets/icons/UpcomingIcons/Time.png";
 
 const Register = (props) => {
+  const monthNames = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
 
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-
-    const FormatDate = (date) => {
-        const year = date.getFullYear()
-        const month = date.getMonth()
-        const day = date.getDay()
-        let days = day;
-        if (day < 10){
-            days = `0${days}`
-        } else {
-            days = day
-        }
-        const dateFormat = `${days} ${monthNames[month]} ${year}`;
-        return dateFormat
+  const FormatDate = (date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDay();
+    let days = day;
+    if (day < 10) {
+      days = `0${days}`;
+    } else {
+      days = day;
     }
+    const dateFormat = `${days} ${monthNames[month]} ${year}`;
+    return dateFormat;
+  };
 
-    const FormatTime = (time) => {
-        const hour = time.getHours()
-        const minute = time.getMinutes()
-        let minutes = minute;
-        if (minute < 10){
-            minutes = `0${minute}`
-        } else {
-            minutes = minute
-        }
-        const timeFormat = `${hour}.${minutes}`;
-        return timeFormat
+  const FormatTime = (time) => {
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    let minutes = minute;
+    if (minute < 10) {
+      minutes = `0${minute}`;
+    } else {
+      minutes = minute;
     }
+    const timeFormat = `${hour}.${minutes}`;
+    return timeFormat;
+  };
 
-    const FormatDateEvent = () => {
-        const date = new Date(props.scheduled_end) 
-        const dateEvent = FormatDate(date)
-        return dateEvent
-    }
+  const FormatDateEvent = () => {
+    const date = new Date(props.scheduled_end);
+    const dateEvent = FormatDate(date);
+    return dateEvent;
+  };
 
-    const FormatTimeEvent = () => {
-        const date = new Date(props.scheduled_end) 
-        const TimeEvent = FormatTime(date)
-        return TimeEvent
-    }
+  const FormatTimeEvent = () => {
+    const date = new Date(props.scheduled_end);
+    const TimeEvent = FormatTime(date);
+    return TimeEvent;
+  };
 
-    return (
-        <Fragment>
-            <div className="register">
-                <p><img src={iconDate} alt="iconDate" />{FormatDateEvent()}</p>
-                <p><img src={iconTime} alt="iconTime" />{FormatTimeEvent()} WIB</p>
-                <p><img src={iconPayments} alt="iconPaid" />{props.fee}</p>
-                {/* <p><img src={iconLocation} alt="iconLocation" />  Live via Zoom Meet</p> */}
-            </div>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <div className="register">
+        <p>
+          <img src={iconDate} alt="iconDate" />
+          {FormatDateEvent()}
+        </p>
+        <p>
+          <img src={iconTime} alt="iconTime" />
+          {FormatTimeEvent()} WIB
+        </p>
+        <p>
+          <img src={iconPayments} alt="iconPaid" />
+          {props.fee}
+        </p>
+        {/* <p><img src={iconLocation} alt="iconLocation" />  Live via Zoom Meet</p> */}
+      </div>
+    </Fragment>
+  );
+};
 
-export default Register
+export default Register;
