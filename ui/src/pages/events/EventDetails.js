883cf4eb414eaeaa0686285a2c3dd8f5c64b74a0
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2021-11-18 20:10:39 +0700

    Add SEO for event details (#108)

--- ui/src/pages/events/EventDetails.js
+++ ui/src/pages/events/EventDetails.js
@@ -3,22 +3,36 @@ import { useRequest } from "ahooks";
 import { Skeleton } from "antd";
 import EventDetailsDone from "./EventDetailsDone";
 import EventDetailsUpcoming from "./EventDetailsUpcoming";
+import { Helmet } from "react-helmet";
 
 const EventDetails = ({ id }) => {
   const { data: event } = useRequest(`/api/v1/events/${id}`);
 
   return (
-    <div className="container">
-      {event ? (
-        new Date(event.scheduled_start) > Date.now() ? (
-          <EventDetailsUpcoming event={event} />
-        ) : (
-          <EventDetailsDone event={event} />
-        )
-      ) : (
-        <Skeleton active />
+    <>
+      {event && (
+        <Helmet>
+          <title>{event.title}</title>
+
+          <meta property="og:description" content={event.description} />
+          <meta property="og:image" content={event.image_url} />
+
+          <meta property="twitter:description" content={event.description} />
+          <meta property="twitter:image" content={event.image_url} />
+        </Helmet>
       )}
-    </div>
+      <div className="container">
+        {event ? (
+          new Date(event.scheduled_start) > Date.now() ? (
+            <EventDetailsUpcoming event={event} />
+          ) : (
+            <EventDetailsDone event={event} />
+          )
+        ) : (
+          <Skeleton active />
+        )}
+      </div>
+    </>
   );
 };
 
