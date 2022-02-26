0eab3d1f4b3749fb17395868a8679408a5ca538d
Faisal Morensya <faisalmorensya@gmail.com>
2022-02-26 10:48:05 +0700

    add new community logo

--- ui/src/pages/landing/Sponsors.js
+++ ui/src/pages/landing/Sponsors.js
@@ -12,13 +12,15 @@ const Landing = () => {
     "https://res.cloudinary.com/patal/image/upload/c_scale,w_300/v1598716749/patal/sponsors/andalas_global_teknologi.png",
     "https://res.cloudinary.com/patal/image/upload/c_scale,w_300/v1598716749/patal/sponsors/my_office.png",
     "https://res.cloudinary.com/patal/image/upload/c_scale,w_300/v1598716749/patal/sponsors/sriwijaya_host.png",
+    "https://res.cloudinary.com/patal/image/upload/v1645847080/patal/sponsors/logo1_ojw3qb.svg",
+    "https://res.cloudinary.com/patal/image/upload/v1645847219/patal/sponsors/PB_AWS_logo_RGB_stacked_REV_SQ.91cd4af40773cbfbd15577a3c2b8a346fe3e8fa2_mfcojl.png"
   ];
 
   return (
     <div style={{ paddingBottom: 80, paddingTop: 80 }}>
       <div className="container center">
         <Title level={3} style={{ marginBottom: 40 }}>
-          Sponsor
+          Supported by:
         </Title>
 
         <Row align="middle" justify="center" gutter={[32, 48]}>
