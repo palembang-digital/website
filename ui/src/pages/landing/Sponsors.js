b962d6fef21da45caaa3bc43b79e40e9d8e94995
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2022-10-22 13:26:36 +0700

    Add microsoft logo (#128)

--- ui/src/pages/landing/Sponsors.js
+++ ui/src/pages/landing/Sponsors.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
 import { Col, Row, Typography } from "antd";
+import React, { useContext } from "react";
 
 import SiteContext from "../../providers/site/SiteContext";
 
@@ -12,8 +12,9 @@ const Landing = () => {
     "https://res.cloudinary.com/patal/image/upload/c_scale,w_300/v1598716749/patal/sponsors/andalas_global_teknologi.png",
     "https://res.cloudinary.com/patal/image/upload/c_scale,w_300/v1598716749/patal/sponsors/my_office.png",
     "https://res.cloudinary.com/patal/image/upload/c_scale,w_300/v1598716749/patal/sponsors/sriwijaya_host.png",
-    "https://res.cloudinary.com/patal/image/upload/v1645847080/patal/sponsors/logo1_ojw3qb.svg",
-    "https://res.cloudinary.com/patal/image/upload/v1645847219/patal/sponsors/PB_AWS_logo_RGB_stacked_REV_SQ.91cd4af40773cbfbd15577a3c2b8a346fe3e8fa2_mfcojl.png"
+    "https://res.cloudinary.com/patal/image/upload/c_scale,w_300/v1645847080/patal/sponsors/logo1_ojw3qb.svg",
+    "https://res.cloudinary.com/patal/image/upload/c_scale,w_300/v1645847219/patal/sponsors/PB_AWS_logo_RGB_stacked_REV_SQ.91cd4af40773cbfbd15577a3c2b8a346fe3e8fa2_mfcojl.png",
+    "https://res.cloudinary.com/patal/image/upload/c_scale,w_300/v1666418976/patal/sponsors/microsoft_znnkhn.png",
   ];
 
   return (
@@ -26,7 +27,7 @@ const Landing = () => {
         <Row align="middle" justify="center" gutter={[32, 48]}>
           {sponsors.map((sponsor, idx) => (
             <Col key={idx} span={isMobile ? 12 : 4}>
-              <img alt={sponsor} src={sponsor} height={30} width={100} />
+              <img alt={sponsor} src={sponsor} height={48} />
             </Col>
           ))}
         </Row>
